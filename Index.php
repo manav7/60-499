0df0de22f9306d28316cd612ee7f09b850dcd944
Manav Chugh <manav77.chugh@gmail.com>
2017-11-28 22:20:13 -0600

    Initial commit

---
+++ Index.php
@@ -0,0 +1,47 @@
+<?php?>
+
+<html> 
+     <head>
+	       <title>This is a DataWarehouse Quering System Prototype</title>
+		   <link rel = "stylesheet" type = "text/css" href = "mystyle.css">
+	 <head>
+	 
+	       <body>
+		        <h1 align="center"> Data Warehouse Quering System </h1>
+				
+				<img src="Image.jpg">
+				
+				<div id ="queries">
+					<p><a href = "">View all sales order</a></p>
+				</div>
+				
+				<div id ="queries">
+					<p><a href = "">View total sales</a></p>
+				</div>
+				
+				<div id ="queries">
+					<p><a href = "">View all sales per product by year</a></p>
+				</div>
+				
+				<div id ="queries">
+					<p><a href = "">View all sales per country</a></p>
+				</div>
+				
+				<div id ="queries">
+					<p><a href = "">View all oreders cancelled</a></p>
+				</div>
+				
+				<div id ="queries">
+					<p><a href = "">View all orders in shipment</a></p>
+				</div>
+				
+				<div id ="queries">
+					<p><a href = "">View shipment status of all the orders</a></p>
+				</div>
+						
+		   </body>
+		   
+</html>
+			
+		         
+				 
