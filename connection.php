72ea3b56f1264f5cbcf72f4ff01abd1ba79ecb1f
Manav Chugh <manav77.chugh@gmail.com>
2017-12-02 17:51:40 -0600

    new

--- connection.php
+++ connection.php
@@ -1,11 +1,26 @@
 <?php
 
  /*establish database connection with database information*/
+ $arrLocal_IP = array("127.0.0.1", "::1"); // ip address for local servers
+if(in_array($_SERVER["REMOTE_ADDR"], $arrLocal_IP)){
+	//echo "On local server: apache";
+	$db_username = "root";
+	$db_name = "project_499";
+	$host = "localhost";
+	$password = "wils1996";
+}
+else{
+	//echo "On school server: myweb";
+	$db_username = "chugh11_499";
+	$db_name = "chugh11_project499";
+	$host = "localhost";
+	$password = "warehouse";
 
- $db_username = "root";
- $db_name = "project_499";
- $host = "localhost";
- $password = "wils1996";
+}
+ // $db_username = "root";
+ // $db_name = "project_499";
+ // $host = "localhost";
+ // $password = "wils1996";
 
  $connection = mysqli_connect($host, $db_username, $password, $db_name);
 
